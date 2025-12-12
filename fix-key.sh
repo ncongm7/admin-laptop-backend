0a5cea91dab5caab0a2be57d1c25435e10dbf203
ncongm7 <nguyencong21052002@gmail.com>
2025-12-12 22:46:59 +0700

    tam on

---
+++ fix-key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FILE="src/main/resources/application.properties"
+if [ -f "$FILE" ]; then
+    sed -i 's/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//g' "$FILE"
+    sed -i 's/\${GROQ_API_KEY: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}/\${GROQ_API_KEY:}/g' "$FILE"
+fi
+
---
+++ remove-key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to remove Groq API key from git history
+FILE="src/main/resources/application.properties"
+if [ -f "$FILE" ]; then
+    sed -i 's/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//g' "$FILE"
+    sed -i 's/${GROQ_API_KEY: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}/${GROQ_API_KEY:}/g' "$FILE"
+fi
+
